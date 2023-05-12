9885dbfb765103eed6598a92194ee2366b3c0980
Andrii Lutsiak <timechain04@gmail.com>
2023-05-12 14:50:57 +0300

    npm

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    res.json({
+        sucess: true,
+    })
+})
+
+app.listen(4444, (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log('Server OK');
+})
