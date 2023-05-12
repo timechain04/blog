8aabc73624785d182e8808304951b9df3478ca3a
Andrii Lutsiak <timechain04@gmail.com>
2023-05-12 19:51:13 +0300

    npm mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express()
 
@@ -10,10 +11,20 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
     console.log(req.body);
+
+    if (req.body.email === 'test@test.ru') {
+        const token = jwt.sign({
+            email: req.body.email,
+            fullName: 'John Feynman',
+        }, 
+        'secret123',
+        );
+    }
     res.json({
         sucess: true,
-    })
-})
+        token,
+    });
+});
 
 app.listen(4444, (err) => {
     if(err) {
